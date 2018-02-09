c72e26fc9d469dc220220942f9c8b2e07be26262
Greg Bowler <greg.bowler@g105b.com>
2018-02-09 13:46:18 +0000

    Update index.php

--- www/logbox/index.php
+++ www/logbox/index.php
@@ -1,2 +1,31 @@
-<?php
-die("Logbox, updated from original Git commit.");
+<!doctype html>
+<html>
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Logbox settings</title>
+
+        <style>
+        html {
+                height: 100%;
+        }
+        body {
+                margin: 0;
+                height: 100%;
+                overflow: hidden;
+                letter-spacing: -5px;
+        }
+        iframe {
+                width: 100%;
+                height: 33.333%;
+                border: none;
+        }
+        </style>
+</head>
+<body>
+
+<iframe id="raw" src="/raw.php"></iframe>
+<iframe src="/settings.php"></iframe>
+<iframe src="/output.php"></iframe>
+</body>
+</html>
