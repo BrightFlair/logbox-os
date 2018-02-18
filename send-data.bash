7e419fc2809804686be1c014a78f315627152689
Greg Bowler <greg.bowler@g105b.com>
2018-02-18 18:20:56 +0000

    Send data to subdir

--- send-data.bash
+++ send-data.bash
@@ -2,5 +2,5 @@
 temp=$(mktemp /tmp/logbox-data.XXXXX)
 host=$(hostname)
 /home/logbox/os/get-data.php > $temp
-scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox $temp logbox@dhp.srv.brightflair.com:~/$host.txt
+scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox $temp logbox@dhp.srv.brightflair.com:~/data/$host.txt
 rm $temp
