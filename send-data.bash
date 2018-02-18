d30de29a2c02fa938e7efe58cf075a6d85cf9be1
Greg Bowler <greg.bowler@g105b.com>
2018-02-18 18:42:40 +0000

    Send to ACL directory

--- send-data.bash
+++ send-data.bash
@@ -2,5 +2,5 @@
 temp=$(mktemp /tmp/logbox-data.XXXXX)
 host=$(hostname)
 /home/logbox/os/get-data.php > $temp
-scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox $temp logbox@dhp.srv.brightflair.com:~/data/$host.txt
+scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox $temp logbox@dhp.srv.brightflair.com:/var/www/data.logbox/data/$host.txt
 rm $temp
