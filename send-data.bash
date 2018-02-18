5518c5b21d886a27355c86e37f624c5f7e7211ca
Greg Bowler <greg.bowler@g105b.com>
2018-02-18 18:18:03 +0000

    Send data every 5 minutes

--- send-data.bash
+++ send-data.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
-lynx -dump -nolist http://localhost/output.php | sed -e 's/.*REFRESH.*//'
-scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox /tmp/output logbox@dhp.srv.brightflair.com:~/.
+temp=$(mktemp /tmp/logbox-data.XXXXX)
+host=$(hostname)
+/home/logbox/os/get-data.php > $temp
+scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox $temp logbox@dhp.srv.brightflair.com:~/$host.txt
+rm $temp
