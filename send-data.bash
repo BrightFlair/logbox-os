3d3a089e3735eacc3cfce96684f32ef811d73956
Greg Bowler <greg.bowler@g105b.com>
2018-02-18 20:02:21 +0000

    Persist permissions after copy

--- send-data.bash
+++ send-data.bash
@@ -3,4 +3,5 @@ temp=$(mktemp /tmp/logbox-data.XXXXX)
 host=$(hostname)
 /home/logbox/os/get-data.php > $temp
 scp -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox $temp logbox@dhp.srv.brightflair.com:/var/www/data.logbox/data/$host.txt
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_logbox logbox@dhp.srv.brightflair.com "chmod 644 /var/www/data.logbox/data/$host.txt"
 rm $temp
