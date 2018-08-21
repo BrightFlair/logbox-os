35332c1fc75c8247832a70a9f7ebddbd0a2ce55d
Greg Bowler <greg.bowler@g105b.com>
2018-08-21 22:49:58 +0100

    Strip string

--- get-latest-counters.py
+++ get-latest-counters.py
@@ -10,14 +10,14 @@ if(not os.path.isfile(c1filename)):
 	
 c1 = 0
 with open(c1filename) as c1in:
-	c1 = c1in.read()
+	c1 = c1in.read().strip()
 	
 c2 = 0
 with open(c2filename) as c2in:
-	c2 = c2in.read()
+	c2 = c2in.read().strip()
 	
-c2 = 0
-with open(c2filename) as c2in:
-	c2 = c2in.read()
+c3 = 0
+with open(c3filename) as c3in:
+	c3 = c3in.read().strip()
 	
 print("C1:" + c1 + " C2:" + c2 + " C3:" + c3)
