007d5b8b53173d7ecb0a98375f5683f2170257e8
Greg Bowler <greg.bowler@g105b.com>
2020-07-20 22:30:12 +0100

    Ensure counters are int values

--- get-latest-counters.py
+++ get-latest-counters.py
@@ -7,17 +7,29 @@ c3filename = "/home/logbox/c3"
 if(not os.path.isfile(c1filename)):
 	print("C1:0 C2:0 C3:0")
 	exit()
-	
+
 c1 = 0
 with open(c1filename) as c1in:
-	c1 = c1in.read().strip()
-	
+	c1String = c1in.read().strip()
+try:
+	c1 = int(c1String)
+except:
+	pass
+
 c2 = 0
 with open(c2filename) as c2in:
-	c2 = c2in.read().strip()
-	
+	c2String = c2in.read().strip()
+try:
+	c2 = int(c2String)
+except:
+	pass
+
 c3 = 0
 with open(c3filename) as c3in:
-	c3 = c3in.read().strip()
-	
-print("C1:" + c1 + " C2:" + c2 + " C3:" + c3)
+	c3String = c3in.read().strip()
+try:
+	c3 = int(c3String)
+except:
+	pass
+
+print("C1:%d C2:%d C3:%d" % (c1, c2, c3))
